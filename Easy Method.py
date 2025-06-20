ceb4d0b2266147f53488f32b3f515df5b8997437
tajulislamsaidul <tajulislam2103@gmail.com>
2025-06-20 21:24:56 +0600

    Easy Method.py

--- Easy Method.py
+++ Easy Method.py
@@ -1,8 +1,6 @@
 import subprocess
-
 print(f"{'Wi-Fi Name':<30} | Password")
 print("-" * 45)
-
 profiles = subprocess.getoutput("netsh wlan show profiles")
 profiles = [line.split(":")[1].strip() for line in profiles.splitlines() if "All User Profile" in line]
 
@@ -11,5 +9,4 @@ for name in profiles:
     pwd_lines = [line for line in result.splitlines() if "Key Content" in line]
     password = pwd_lines[0].split(":")[1].strip() if pwd_lines else ""
     print(f"{name:<30} | {password}")
-
-input("\nPress Enter to exit...")
+input("\nPress Enter to exits...")
